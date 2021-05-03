ad074c27b1795d6dbfd15fb3a56300fa00c42150
Krokus-lab <piotrus991@wp.pl>
2021-05-04 00:36:57 +0200

    Small Fix
    got rid of the models that i dont need anymore

--- MVCSHOP/MVCSHOP/Models/Opinia.cs
+++ MVCSHOP/MVCSHOP/Models/Opinia.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,6 +11,8 @@ namespace MVCSHOP.Models
         public int IdOpinia { get; set; }
         public int IdKlient { get; set; }
         public int IdPlyta { get; set; }
+        [Required(AllowEmptyStrings = false)]
+        [StringLength(50, ErrorMessage = "Za długa tresc")]
         public string Tresc { get; set; }
     }
 }
